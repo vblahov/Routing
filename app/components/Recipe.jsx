cdbc09b9c63760d5c6da6f811535622009397da9
holyfair <40454487+holyfair@users.noreply.github.com>
2020-03-29 15:19:50 +0200

    commit

--- app/components/Recipe.jsx
+++ app/components/Recipe.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Link, Route, Switch } from 'react-router-dom';
     
 export default function Recipe (props) {
   const url = "/recipes/" + 1;
-  return <div className="recipe">
+  return <Link to={url}>
+        <div className="recipe">
           <h4 className="recipe-name">{props.recipe.name}</h4>
           <h2 className="recipe-category">{props.recipe.category}</h2>
           <img src={props.recipe.picture} className="recipeImg"></img>
@@ -11,4 +12,5 @@ export default function Recipe (props) {
           <p>{props.recipe.longDesc}</p>
           <h5>Date of creating: {props.recipe.createDate}</h5>
          </div>
+         </Link>;
 }
